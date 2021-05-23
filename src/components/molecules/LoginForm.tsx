b00bda66c16d3cbf85e7f4af2cb7b27af32dd0eb
vihong <probamaths@gmail.com>
2021-05-23 14:48:48 +0200

    💄 Add styling to form

--- src/components/molecules/LoginForm.tsx
+++ src/components/molecules/LoginForm.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 export default function LoginForm() {
 	return (
-		<div>
-			<h1>LoginForm</h1>
+		<form>
+			<h1>Please log in</h1>
+
 			<label htmlFor="username">Username:</label>
 			<input id="username" />
 
 			<label htmlFor="password">Password:</label>
 			<input id="password" />
-			<button>Submit</button>
-		</div>
+			<button type="submit">Submit</button>
+		</form>
 	)
 }
