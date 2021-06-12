6da1e400b2bd53563bfae4ed7b2ef9c9512b688b
vihong <probamaths@gmail.com>
2021-06-13 01:08:21 +0200

    ♻️ Clean setup

--- src/components/molecules/Login.test.tsx
+++ src/components/molecules/Login.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import LoginForm from './molecules/LoginForm'
+import LoginForm from './LoginForm'
 
 describe('LoginForm being tested', () => {
 	// step 1 : CAN I SEE IT ?
